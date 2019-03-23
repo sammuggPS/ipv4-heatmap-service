afe6397930a4b5d423bd152ce41c8d7a7866fc8c
Sam Muggleworth <sam.muggleworth@globant.com>
2019-03-23 19:33:50 -0400

    Add function to construct SELECT query

--- services/ip-coordinate-service.js
+++ services/ip-coordinate-service.js
@@ -2,10 +2,10 @@
  * Validates input latitudes and longiutdes
  * Returns uneventfully if params are valid; Throws an error if invalid.
  *
- * This was written with TDD!
- * The prefix '_' indicates that this is a private method that shouldn't be exported.
  *
  * @param {*} parameters - query parameters to validate; sourced from req.query
+ * @throws error if params are invalid
+ * @returns undefined
  */
 const _validateBoundingBoxCoordinates = (parameters) => {
   if (!parameters ||
@@ -17,6 +17,7 @@ const _validateBoundingBoxCoordinates = (parameters) => {
     };
   }
 
+  // Taking advantage of Javascript where 1 == '1' (even though 1 !== '1')
   if (!(-90 <= parameters.upperlat && 90 >= parameters.upperlat &&
     -90 <= parameters.lowerlat && 90 >= parameters.lowerlat &&
     -179.999999<= parameters.upperlong && 180 >= parameters.upperlong &&
@@ -26,6 +27,30 @@ const _validateBoundingBoxCoordinates = (parameters) => {
       message: 'upperlat, lowerlat, upperlong, and lowerlong should be in valid geospatial range'
     };
   }
+};
+
+/**
+ * Constructs the SELECT query that uses ST_INTERSECT to find points in the bound box.
+ * - ipv4_points is the table containing the coordinate data from GeoLite2-City-Blocks-IPv4.csv
+ * - p.geo is the GEOGRAPHY type column in PG
+ * - The POLYGON defines the area in which to find coordinates
+ * - ST_GeomFromText lets us form a POLYGON on the fly (doesn't have to be stored in db)
+ * - the ST_INTERSECTS will return true if the point is inside the polygon
+ *
+ * The prefix '_' indicates that this is a private method that shouldn't be exported.
+ *
+ * @param {*} params - dereferenced upper and lower latitude and longitude
+ * @returns {String} SELECT query
+ */
+const _constructIntersectQuery = ({ upperlat, upperlong, lowerlat, lowerlong}) => {
+  return 'SELECT * FROM ipv4_points as p WHERE ' +
+    'ST_INTERSECTS(ST_GeomFromText(\'POLYGON((' +
+      `${lowerlong} ${lowerlat}, ` +
+      `${lowerlong} ${upperlat}, ` +
+      `${upperlong} ${upperlat}, ` +
+      `${upperlong} ${lowerlat}, ` +
+      `${lowerlong} ${lowerlat}))', 4326), ` +
+      'p.geo)';
 };
 
 module.exports = {};
